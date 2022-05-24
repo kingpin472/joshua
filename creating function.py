1585e96e1ed0438fb378514d966436a3eb9a0ed6
kingpin472 <105699037+kingpin472@users.noreply.github.com>
2022-05-24 23:47:02 +0300

    Update creating function.py

--- creating function.py
+++ creating function.py
@@ -1,5 +1,9 @@
-def sayhi(name):
-    print("Hello " + name)
-sayhi("mike")
-name= " mike "
-print(" Hello ")
+def add(a,b):
+    return a+b
+result=add(6,7)    
+print(result)
+
+def cube(x):
+    return x*x*x
+result=cube(9)   
+print(result) 
